b1096b4b84b0438c1738ec72cb83725fde7c63a6
Denys Pavlyuk <densf2@gmail.com>
2017-04-09 21:52:24 +0300

    Add files via upload
    files for working fuction python

---
+++ wordcount_les_2.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Упражнение "Количество слов"
+
+Функция main() ниже уже определена и заполнена. Она вызывает функции 
+print_words() и print_top(), которые вам нужно заполнить.
+
+1. Если при вызове файла задан флаг --count, вызывается функция 
+print_words(filename), которая подсчитывает, как часто каждое слово встречается 
+в тексте и выводит:
+слово1 количество1
+слово2 количество2
+...
+
+Выводимый список отсортируйте в алфавитном порядке. Храните все слова 
+в нижнем регистре, т.о. слова "Слон" и "слон" будут обрабатываться как одно 
+слово.
+
+2. Если задан флаг --topcount, вызывается функция print_top(filename),
+которая аналогична функции print_words(), но выводит только топ-20 наиболее 
+часто встречающихся слов, таким образом первым будет самое часто встречающееся 
+слово, за ним следующее по частоте и т.д.
+
+Используйте str.split() (без аргументов), чтобы разбить текст на слова.
+
+Отсекайте знаки припинания при помощи str.strip() с знаками припинания 
+в качестве аргумента.
+
+Совет: не пишите всю программу сразу. Доведите ее до какого-то промежуточного 
+состояния и выведите вашу текущую структуру данных. Когда все будет работать 
+как надо, перейдите к следующему этапу.
+
+Дополнительно: определите вспомогательную функцию, чтобы избежать дублирования 
+кода внутри print_words() и print_top().
+
+"""
+
+import sys
+
+# +++ваш код+++
+# Определите и заполните функции print_words(filename) и print_top(filename).
+# Вы также можете написать вспомогательную функцию, которая читает файл,
+# строит по нему словарь слово/количество и возвращает этот словарь.
+# Затем print_words() и print_top() смогут просто вызывать эту вспомогательную функцию.
+
+###
+count = {}
+
+def return_item_min1(item):
+    return item[-1]
+
+def build_dict(filename):
+    f = open(filename, 'rU', 'utf-8')
+    slova = f.read().split()
+
+    for word in slova:
+        word = word.lower()
+        if word not in count:
+            count[word] = 1
+        else:
+            cout[word] += 1
+
+    f.close()
+
+    return dict
+
+
+def print_words(filename):
+    dict = build_dict(filename)
+
+    for word in sorted(dict.keys()):
+        print word, dict[word]
+
+def print_top(filename):
+    count = build_dict(filename)
+    i = 0
+
+    items = sorted(count, key=return_item_min1, reverse=True)
+    for item in items[:20]:
+        print item[0] + ':' + str(item[1])
+        i += 1
+
+# Это базовый код для разбора аргументов коммандной строки.
+# Он вызывает print_words() и print_top(), которые необходимо определить.
+def main():
+    if len(sys.argv) != 3:
+        print('usage: python wordcount.py {--count | --topcount} file')
+        sys.exit(1)
+
+    option = sys.argv[1]
+    filename = sys.argv[2]
+    if option == '--count':
+        print_words(filename)
+    elif option == '--topcount':
+        print_top(filename)
+    else:
+        print('unknown option: ' + option)
+    sys.exit(1)
+
+if __name__ == '__main__':
+    main()
